270860ee513848861ca9556de8a0f7760dc02749
Andy Klimczak <andyklimczak@fastmail.com>
2017-01-29 21:49:05 -0500

    add api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :stories
-  resources :movies
+  scope :api do
+    resources :stories
+    resources :movies
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
